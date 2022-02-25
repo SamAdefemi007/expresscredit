c042bc8fccf2f80a9d29aee6193e8ee2d0fccf0c
Samuel Olaniyan <olaniyansamuel2015@gmail.com>
2022-02-25 16:25:14 +0000

    index page design

--- expresscredit.py
+++ expresscredit.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
